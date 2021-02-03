3a35de104069b34d1f8bec97a6c37f237a0eee2e
Ihor <ihor.sudom@gmail.com>
2021-02-03 19:49:13 +0200

    option

--- lesson-2-redux-intro/task-2/src/index.js
+++ lesson-2-redux-intro/task-2/src/index.js
@@ -24,8 +24,8 @@ resetBtn.addEventListener('click', onReset);
 
 store.subscribe(() => {
   const state = store.getState();
-  const currentValue = state.history.reduce((acc, value) => acc + value, 0);
-  const historyStryng = state.history.map((item) => item === 1 ? '+1' : item).join(' ');
+  const currentValue = state.history.reduce((acc, value) => acc + +value, 0);
+  const historyStryng = state.history.join(' ');
 
   resultElem.textContent =
     state.history.length === 0 ? '' : `${historyStryng} = ${currentValue}`;
