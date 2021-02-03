77cca066abd3ea4c84979de25aba293a61c93630
Ihor <ihor.sudom@gmail.com>
2021-02-03 15:47:17 +0200

    option

---
+++ lesson-1-react-context/task-2-hw/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from './Header';
+import { users, UserDataContext } from './user-context';
+
+class App extends React.Component {
+  state = {
+    userData: {
+      name: 'Nikola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    },
+  };
+
+  render() {
+
+    return (
+      <UserDataContext.Provider value={this.state.userData}>
+        <div className="page">
+          <Header />
+        </div>
+      </UserDataContext.Provider>
+      
+    );
+  }
+}
+
+export default App;
---
+++ lesson-1-react-context/task-2-hw/src/user-context.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const users = {};
+
+export const UserDataContext = React.createContext(users);
