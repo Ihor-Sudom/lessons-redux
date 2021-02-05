6fa4642a9a3bdfeaf549c2fa56a2f20099ef6c55
Ihor <ihor.sudom@gmail.com>
2021-02-05 17:47:29 +0200

    option

--- lesson-3-combine-reducers/task-3-hw/src/index.js
+++ lesson-3-combine-reducers/task-3-hw/src/index.js
@@ -7,13 +7,10 @@ store.subscribe(() => {
   console.log(store.getState())
 });
 
-console.log(store.getState())
-
-
 store.dispatch(setLanguage('ua'));
-//store.dispatch(addProduct({id: 2, name: 'milk'}));
-//store.dispatch(addProduct({id: 5, name: 'bear'}));
+store.dispatch(addProduct({id: 2, name: 'milk'}));
+store.dispatch(addProduct({id: 5, name: 'bear'}));
 store.dispatch(setUser({name: 'Tom'}));
-//store.dispatch(setUser({name: 'Bob'}));
-//store.dispatch(removeProduct(2));
-//store.dispatch(removeUser());
+store.dispatch(setUser({name: 'Bob'}));
+store.dispatch(removeProduct(2));
+store.dispatch(removeUser());
