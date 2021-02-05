04308866668bf4045bcb64650b6fdedb8afac3f7
Ihor <ihor.sudom@gmail.com>
2021-02-05 13:55:24 +0200

    option

---
+++ lesson-3-combine-reducers/task-1/src/index.js
@@ -0,0 +1,20 @@
+import store from './store';
+import { increment, decrement, reset } from './counter.actions';
+import { addUser, deleteUser, updateUser } from './users.actions';
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch(increment(1));
+store.dispatch(decrement(1));
+store.dispatch(increment(1));
+store.dispatch(increment(1));
+store.dispatch(reset());
+
+store.dispatch(addUser({ id: 1, name: 'Tom' }));
+store.dispatch(addUser({ id: 2, name: 'Bob' }));
+
+store.dispatch(deleteUser(1));
+
+store.dispatch(updateUser(2, { name: 'Bob Martin', age: 17 }));
