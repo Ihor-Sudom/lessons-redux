841acafa2bdcef0c587b4a954739253bc24db85b
Ihor <ihor.sudom@gmail.com>
2021-02-08 08:43:10 +0200

    option

---
+++ lesson-6-async-actions/task-1/src/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import counterReducer from './counter.reducer';
+
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+}
+
+const store = createStore(
+  counterReducer,
+  applyMiddleware(logger),
+);
+
+export default store;
